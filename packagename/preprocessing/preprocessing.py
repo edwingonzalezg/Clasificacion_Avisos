8de25bf66b293d75ba471109e2731b2c8beccceb
Edwin Yecid González Gómez <ingeniero.ed@gmail.com>
2025-08-13 13:04:18 -0500

    Actualizacion

--- packagename/preprocessing/preprocessing.py
+++ packagename/preprocessing/preprocessing.py
@@ -1,18 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.14.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
 
-# %%
 import re
 import pandas as pd
 import numpy as np
